6877180bb73aa007059a39fd71c9ff37d55e31ac
makkul1 <91888434+makkul1@users.noreply.github.com>
2021-11-09 14:53:31 +0100

    Update SuperAddition.java

--- src/main/java/pl/edu/amu/seclasses5/SuperAddition.java
+++ src/main/java/pl/edu/amu/seclasses5/SuperAddition.java
@@ -60,9 +60,9 @@ public class SuperAddition {
 class isNumeric {
     public static boolean isNumeric(String str)
     {
-    for (char c : str.toCharArray()) 
+    for (char c : str.toCharArray())
     {
-        if (!Character.isDigit(c) && c != 45) return false;
+        if (!Character.isDigit(c) && c!=45) return false;
     }
     return true;
     }
